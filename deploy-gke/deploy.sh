2e0b138faa0d2242785a7e252518343c8b355149
Julian Aimar <julianaimar.dxc@latam.com>
2021-06-13 00:13:49 -0300

    deploy.sh

--- deploy-gke/deploy.sh
+++ deploy-gke/deploy.sh
@@ -6,10 +6,13 @@ set -ef -o pipefail
 # Activate trace if needed
 [ "$TRACE" = "true" ] && set -x
 
+GCP_info="deploy-gke/GCP.info"
+APP_YAML="deploy-gke/app.yaml"
+
 # Use proper credentials according to environment
-GKE=`grep GKE GCP.info | cut -f2 -d:`
-REGION=`grep REGION GCP.info | cut -f2 -d:`
-PROJECT=`grep PROJECT GCP.info | cut -f2 -d:`
+GKE=`grep GKE ${GCP_info} | cut -f2 -d:`
+REGION=`grep REGION ${GCP_info} | cut -f2 -d:`
+PROJECT=`grep PROJECT ${GCP_info} | cut -f2 -d:`
 
 VERSION=$1
 
@@ -21,5 +24,5 @@ sed -i "s/APP/${IMAGE_NAME}/g; s/VERSION/${VERSION}/g" app.yaml
 gcloud auth activate-service-account --key-file=${GOOGLE_CRED}
 gcloud container clusters get-credentials ${GKE} --region ${REGION} --project ${PROJECT}
 
-cat app.yaml
+cat ${APP_YAML}
 #kubectl apply -f app.yaml
