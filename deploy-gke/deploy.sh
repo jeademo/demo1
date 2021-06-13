250e235ed4b83748158c30ff321f1010dec0fd5f
Julian Aimar <julianaimar.dxc@latam.com>
2021-06-13 00:14:34 -0300

    deploy.sh

--- deploy-gke/deploy.sh
+++ deploy-gke/deploy.sh
@@ -8,8 +8,6 @@ set -ef -o pipefail
 
 GCP_info="deploy-gke/GCP.info"
 APP_YAML="deploy-gke/app.yaml"
-
-# Use proper credentials according to environment
 GKE=`grep GKE ${GCP_info} | cut -f2 -d:`
 REGION=`grep REGION ${GCP_info} | cut -f2 -d:`
 PROJECT=`grep PROJECT ${GCP_info} | cut -f2 -d:`
@@ -19,7 +17,7 @@ VERSION=$1
 #export GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_CRED}
 #export GOOGLE_CLOUD_KEYFILE_JSON=${GOOGLE_CRED}
 
-sed -i "s/APP/${IMAGE_NAME}/g; s/VERSION/${VERSION}/g" app.yaml
+sed -i "s/APP/${IMAGE_NAME}/g; s/VERSION/${VERSION}/g" ${APP_YAML}
 
 gcloud auth activate-service-account --key-file=${GOOGLE_CRED}
 gcloud container clusters get-credentials ${GKE} --region ${REGION} --project ${PROJECT}
